9d6dd9eaed8cfd447a240e53177500d1b611506f
alexmrqr <a.marquestortoza@gmail.com>
2023-10-04 13:57:00 -0300

    Versão web

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,9 @@ export default function Home() {
                 <span className='text-8xl'> {'{'}   </span>
 
                 <nav className='flex flex-col justify-center text-xl text-center text-link gap-1'>
-                    <a href="#" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Sobre Mim</a>
-                    <a href="#" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Portfolio</a>
-                    <a href="#" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Contato</a>
+                    <a href="#sobreMim" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Sobre Mim</a>
+                    <a href="#Portfolio" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Portfolio</a>
+                    <a href="#contato" className=' hover:text-secondary delay-300 hover:delay-75  duration-300 hover:duration-200 ease-in-out'>Contato</a>
                 </nav>
 
                 <span className='text-8xl'> {'}'}   </span>
@@ -35,14 +35,15 @@ export default function Home() {
 
 
         </div>
-        <Image src="/photoPlaceHolder.png" width={300} height={300} alt="Picture of the author"/>
+        {/*  TIRAR UMA FOTO BOA */}
+        {/* <Image src="/profilePic.jpg" width={300} height={300} alt="Picture of the author"/> */}
     </div>
 
 
     <div className='m-14 gap-14 flex justify-around'>
 
         <div className='max-w-lg'>
-            <h3 className='text-5xl text-secondary'>Sobre Mim</h3>
+            <h3 className='text-5xl text-secondary'id='sobreMim' >Sobre Mim</h3>
             <p className='text-xl '>Desde jovem, sempre fui fascinado por tecnologia. Em 2021, dei início à minha jornada na programação, começando pelo front-end, onde me identifiquei e evoluí rapidamente. Em 2022, ingressei na PUCPR, onde consegui aprimorar minhas habilidades. Logo após, tornei-me um desenvolvedor fullStack.</p>
         </div>
 
@@ -63,6 +64,7 @@ export default function Home() {
         <div className='flex  gap-2 pt-1'>
             <Image alt='Logo - CSharp' src="/csharp-line.svg" width={60} height={60}></Image>
             <Image alt='Logo - DotNet' src="/dot-net-plain.svg" width={60} height={60}></Image>
+            <Image alt='Logo - PHP' src="/php-plain.svg" width={60} height={60}></Image>
             <Image alt='Logo - MySQL' src="/mysql-plain-wordmark.svg" width={60} height={60}></Image>
         </div>
             <h6 className='font-extralight'>Conheço</h6>
@@ -78,9 +80,39 @@ export default function Home() {
 
 
 
-    <div>
-        <h3 className='text-secondary uppercase text-center text-2xl'>Projetos</h3>
+    <div id='Portfolio' className='flex justify-center flex-col'>
+        <h3 className='text-5xl text-center m-5 text-secondary'>Projetos</h3>
+        <div className='flex justify-around gap-8 flex-wrap'>
 
+            <div className='flex flex-col text-center '>
+                <h4 className='uppercase'>Lista Universitária</h4>
+                <Image className='border border-secondary' alt='Lista img' src="/listaUniversitarias.png" width={300} height={60}></Image>    
+            </div>
+            
+            <div className='flex flex-col text-center '>
+                <h4 className='uppercase'>Naut Tech</h4>
+                <Image className='border border-secondary' alt='Lista img' src="/NautTech.png" width={300} height={60}></Image>    
+            </div>
+            
+
+            <div className='flex flex-col text-center '>
+                <h4 className='uppercase'>Honey Game</h4>
+                <Image className='border border-secondary' alt='Lista img' src="/HoneyGames.png" width={300} height={60}></Image>    
+            </div>
+            
+            
+            
+
+        </div>
+    </div>
+
+    <div id='contato' className='p-20 '>
+        <h3 className='text-5xl text-center m-5 text-secondary' id='contato'>Contato</h3>
+        <div className='flex gap-5 justify-center'><h6>Email:</h6> <a href="mailto:a.marquestortoza@gmail.com" className='text-accent'>a.marquestortoza@gmail.com</a></div>
+        <div className='flex gap-5 justify-center'><h6>Telefone:</h6> <a href="tel:+5541992081308" className='text-accent'>+55 (41) 99208-1308</a></div>
+        <div className='flex gap-5 justify-center'><h6>Github:</h6> <a href="https://github.com/Alexandre-Tortoza" className='text-accent'> Alexandre-Tortoza </a></div>
+        <div className='flex gap-5 justify-center'><h6>LinkedIn:</h6> <a href="https://www.linkedin.com/in/alexandre-marques-a7195724b/" className='text-accent'> Alexandre Marques </a></div>
+        <div className='flex gap-5 justify-center'><h6>currículo :</h6> <a href="/resume__Alexandre-Marques.pdf" download className='text-accent'>CV Alexandre M.</a></div>
     </div>
     </main>
 )
